9df8e8c9348996ce40b7940b0b100f7cb832f726
roybooty <safwanidris4@proton.me>
2026-01-30 23:35:45 +0100

    updated ticket genration logic

--- apps/safis_backend/controllers/ticket.controller.ts
+++ apps/safis_backend/controllers/ticket.controller.ts
@@ -4,32 +4,101 @@ import query from "../config/database.ts";
 import { eq } from "drizzle-orm";
 import QRCode from "qrcode";
 import { BASE_URL } from "../config/env.ts";
+import { and } from "drizzle-orm";
 
 export const generateTicketGeneral = async (req, res) => {
     try {
+        const { ticketGeneral, ticketVip, ticketGAmount, ticketVAmount } = req.body;
         const id = req.params.id;
 
-        const isAvailable = await query.select().from(events).where(eq(events.id, id) && eq(events.active, true));
+        const isAvailable = await query.select().from(events).where(and(eq(events.id, id), eq(events.active, true)));
+        let special;
 
-        if(isAvailable[0].generalTicket <= 0){
-            const err = new Error()
-            err.message = "General Ticket is finished"
-            err.statusCode = 303;
+        if (ticketGeneral === "General") {
+            if (isAvailable[0].generalTicket <= 0) {
+                const err = new Error()
+                err.message = "General Ticket is finished"
+                err.statusCode = 303;
+            }
+
+            const ticket = await query.select().from(tickets).where(and(eq(tickets.eventId, id), eq(tickets.sold, false), eq(tickets.ticketCode, ""), eq(tickets.type, "General"))).limit(ticketGAmount);
+            if (ticketGAmount > 1) {
+
+                special = [];
+
+                for (let i = 0; i < ticketGAmount; i++) {
+                    special.push(await QRCode.toDataURL(`${BASE_URL}/${ticket[0].id}`));
+                    const newTicket = await query.update(tickets).set({
+                        ticketCode: special[i],
+                        sold: true,
+                    }).where(eq(tickets.id, ticket[0].id))
+
+                    const newAmount = isAvailable[0].generalTicket - 1
+
+                    await query.update(events).set({
+                        generalTicket: newAmount,
+                    }).where(eq(events.id, isAvailable[0].id))
+                }
+            }
+
+            special = await QRCode.toDataURL(`${BASE_URL}/${ticket[0].id}`);
+            const newTicket = await query.update(tickets).set({
+                ticketCode: special,
+                sold: true,
+            }).where(eq(tickets.id, ticket[0].id))
+
+            const newAmount = isAvailable[0].generalTicket - 1
+
+            await query.update(events).set({
+                generalTicket: newAmount,
+            }).where(eq(events.id, isAvailable[0].id))
+
+
+
+        }
+        else if (ticketVip == "Vip") {
+            if (isAvailable[0].vipTicket <= 0) {
+                const err = new Error()
+                err.message = "vip Ticket is finished"
+                err.statusCode = 303;
+            }
+
+            const ticket = await query.select().from(tickets).where(eq(tickets.eventId, id) && eq(tickets.sold, false) && eq(tickets.ticketCode, "") && eq(tickets.type, "Vip")).limit(ticketVAmount);
+
+            if (ticketVAmount > 1) {
+
+                special = [];
+
+                for (let i = 0; i < ticketVAmount; i++) {
+                    special.push(await QRCode.toDataURL(`${BASE_URL}/${ticket[0].id}`));
+                    const newTicket = await query.update(tickets).set({
+                        ticketCode: special[i],
+                        sold: true,
+                    }).where(eq(tickets.id, ticket[0].id))
+
+                    const newAmount = isAvailable[0].vipTicket - 1
+
+                    await query.update(events).set({
+                        generalTicket: newAmount,
+                    }).where(eq(events.id, isAvailable[0].id))
+                }
+            }
+
+            special = await QRCode.toDataURL(`${BASE_URL}/${ticket[0].id}`);
+            const newTicket = await query.update(tickets).set({
+                ticketCode: special,
+                sold: true,
+            }).where(eq(tickets.id, ticket[0].id))
+
+            const newAmount = isAvailable[0].vipTicket - 1
+
+            await query.update(events).set({
+                generalTicket: newAmount,
+            }).where(eq(events.id, isAvailable[0].id))
+
         }
 
-        const ticket = await query.select().from(tickets).where(eq(tickets.eventId, id) && eq(tickets.sold, false) && eq(tickets.ticketCode, "") && eq(tickets.type, "General")).limit(1);
 
-        const special = await QRCode.toDataURL(`${BASE_URL}/${ticket[0].id}`);
-        const newTicket = await query.update(tickets).set({
-            ticketCode: special,
-            sold: true,
-        }).where(eq(tickets.id, ticket[0].id))
-
-        const newAmount = isAvailable[0].generalTicket - 1
-
-        await query.update(events).set({
-            generalTicket: newAmount,
-        }).where(eq(events.id, isAvailable[0].id))
 
         res.status(201).json({
             success: true,
@@ -38,57 +107,19 @@ export const generateTicketGeneral = async (req, res) => {
         })
 
     } catch (e) {
-        res.status(e.statusCode || 500).json({ success: false, message: e})
+        res.status(e.statusCode || 500).json({ success: false, message: e })
         console.log(e)
     }
 }
 
-export const generateTicketVip = async (req, res) => {
-    try {
-        const id = req.params.id;
-
-        const isAvailable = await query.select().from(events).where(eq(events.id, id) && eq(events.active, true));
-
-        if(isAvailable[0].vipTicket <= 0){
-            const err = new Error()
-            err.message = "vip Ticket is finished"
-            err.statusCode = 303;
-        }
-
-        const ticket = await query.select().from(tickets).where(eq(tickets.eventId, id) && eq(tickets.sold, false) && eq(tickets.ticketCode, "") && eq(tickets.type, "Vip")).limit(1);
-
-        const special = await QRCode.toDataURL(`${BASE_URL}/${ticket[0].id}`);
-
-        const newTicket = await query.update(tickets).set({
-            ticketCode: special,
-            sold: true,
-        }).where(eq(tickets.id, ticket[0].id))
-
-        const newAmount = isAvailable[0].vipTicket - 1
-
-        await query.update(events).set({
-            vipTicket: newAmount,
-        }).where(eq(events.id, isAvailable[0].id))
-
-        res.status(201).json({
-            success: true,
-            message: "ticket aquired succesfully",
-            data: special,
-        })
-
-    } catch (e) {
-        res.status(e.statusCode || 500).json({ success: false, message: e})
-        console.log(e)
-    }
-}
 
 export const confirmTicket = async (req, res) => {
-    try{
+    try {
         const id = req.params.id;
 
         const verify = await query.select().from(tickets).where(eq(tickets.id, id));
 
-        if(!verify){
+        if (!verify) {
             const err = new Error()
             err.statusCode = 404;
             err.message = "Ticket invalid"
@@ -100,8 +131,8 @@ export const confirmTicket = async (req, res) => {
             message: "you are alowed to pass"
         })
 
-    }catch (e) {
-        res.status(e.statusCode || 500).json({ success: false, message: e})
+    } catch (e) {
+        res.status(e.statusCode || 500).json({ success: false, message: e })
         console.log(e)
     }
 }
--- apps/safis_backend/routes/ticket.route.ts
+++ apps/safis_backend/routes/ticket.route.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { generateTicketGeneral, generateTicketVip, confirmTicket } from "../controllers/ticket.controller.ts";
+import { generateTicketGeneral, confirmTicket } from "../controllers/ticket.controller.ts";
 
 const ticketRoter = Router()
 
-ticketRoter.post("/generate-general/:id", generateTicketGeneral);
-ticketRoter.post("/generate-vip/:id", generateTicketVip);
+ticketRoter.post("/:id", generateTicketGeneral);
 ticketRoter.get("/:id", confirmTicket)
 
 export default ticketRoter
